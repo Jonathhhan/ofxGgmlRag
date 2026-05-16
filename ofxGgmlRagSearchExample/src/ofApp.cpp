b96b21e00cfaa78d3509f93022dc706a2d5f7def
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2026-05-16 02:21:47 +0200

    Use ImGui in search example
    Use ImGui in search example

--- ofxGgmlRagSearchExample/src/ofApp.cpp
+++ ofxGgmlRagSearchExample/src/ofApp.cpp
@@ -2,6 +2,7 @@
 
 void ofApp::setup() {
 	ofSetWindowTitle("ofxGgmlRag smoke example");
+	gui.setup(nullptr, false);
 	request.query = "where is this idea documented?";
 	status = ofxGgmlRagUtils::describe(request);
 	ofLogNotice("ofxGgmlRagSearchExample") << status;
@@ -9,7 +10,15 @@ void ofApp::setup() {
 
 void ofApp::draw() {
 	ofBackground(18);
-	ofSetColor(240);
-	ofDrawBitmapString("ofxGgmlRag", 32, 48);
-	ofDrawBitmapString(status, 32, 78);
+	gui.begin();
+	ImGui::SetNextWindowPos(ImVec2(24.0f, 24.0f), ImGuiCond_Once);
+	ImGui::SetNextWindowSize(ImVec2(560.0f, 220.0f), ImGuiCond_Once);
+	if (ImGui::Begin("ofxGgmlRag Search Example")) {
+		ImGui::TextUnformatted("Retrieval Request");
+		ImGui::Separator();
+		ImGui::TextWrapped("%s", status.c_str());
+	}
+	ImGui::End();
+	gui.end();
+	gui.draw();
 }
